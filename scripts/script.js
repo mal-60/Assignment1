280f7cbca648d7b590cad76ed51149c5cf1e9193
Anh <minhanhle@hotmail.com>
2022-11-30 20:14:14 -0800

    add function to change textContent of dark button from dark theme and light theme switch

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,19 @@
 const sideBar = document.querySelector(".sidebar");
 const betterNotes = document.querySelector(".better-notes");
-const blueButton = document.querySelector(".blue-theme")
+const blueButton = document.querySelector(".blue-theme");
 const darkButton = document.querySelector(".dark-theme");
 const saveCancel = document.querySelector(".save-cancel");
 const pinkButton = document.querySelector(".pink-theme");
 
 
-function darkButton()
-{
-    darkButton.classList.toggle("dark");
+function textChange () {
+    if (darkButton.textContent === "Dark Theme") {
+        darkButton.textContent = "Light Theme";
+    }
+    else if (darkButton.textContent === "Light Theme") {
+        darkButton.textContent = "Dark Theme";
+    }
 }
+darkButton.addEventListener("click", textChange);
 
-darkButton.addEventListener("click", darkButton);
+// darkButton.classList.toggle("dark");
